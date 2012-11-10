c34e1347de817c8b16a7033a504a213f0790d782
alunny <alunny@gmail.com>
2012-11-10 15:04:44 -0800

    templated repl pages

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var app = require('tako')(),
     path = require('path'),
     fs = require('fs'),
-    bundle = require('./bundle');
+    bundle = require('./bundle'),
+    templates = app.templates,
+    pages = {};
+
+templates.directory(path.join(__dirname, 'templates'));
 
 app.route('/').files(path.join(__dirname, 'static', 'index.html'));
 
@@ -15,6 +19,15 @@ app.route('/js/:module.js', function (req, res) {
         res.writeHead(200, {'Content-Type': 'text/javascript'});
         fs.createReadStream(outputPath).pipe(res);
     });
+}).methods('GET');
+
+app.route('/repl/:module', function (req, res) {
+    var page = app.page();
+
+    page.template('repl.html.mustache')
+    page.results = req.params;
+
+    page.pipe(res);
 }).methods('GET');
 
 app.route('*').files(path.join(__dirname, 'static'));
