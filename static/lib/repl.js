ba4b7bd1f15292c4a662c9e375cb734a950bdb30
alunny <alunny@gmail.com>
2012-11-10 11:27:31 -0800

    basic repl

---
+++ static/lib/repl.js
@@ -0,0 +1,54 @@
+// class for a Repl, given a CodeMirror editor
+function Repl(editor) {
+    this.editor = editor;
+    this.environment = new Env;
+
+    this.editor.repl = this;
+}
+
+Repl.prototype.evaluate = function (input) {
+    return this.environment.eval(input);
+}
+
+var replKeyMap = CodeMirror.keyMap.repl = {
+    'Enter': function (cm) {
+        // evaluate current line in env, print output
+        var currentLine = cm.getCursor().line,
+            outputLine = currentLine + 1,
+            nextLine = outputLine + 1,
+            contents = cm.getLine(currentLine);
+
+        output = cm.repl.evaluate(contents);
+
+        addLines(cm, 2)
+
+        cm.setLine(outputLine, output);
+        cm.setLineClass(outputLine, 'repl_output');
+        cm.setCursor({ line: nextLine, ch: 0 });
+
+        myMirror.setMarker(currentLine, '> ', 'old_cursor');
+        myMirror.setMarker(nextLine, '> ', 'active_cursor');
+    }
+}
+
+function addLines(cm, n) {
+    var lastLine = cm.lineCount() - 1,
+        contents = cm.getLine(lastLine),
+        n = n || 1,
+        nls = [];
+
+    while (n-- > 0) {
+        nls.push('\n');
+    }
+
+    cm.setLine(lastLine, contents + nls.join(''));
+}
+
+function Env() {
+    var _;
+
+    this.eval = function (code) {
+        _ = eval(code);
+        return _.toString();
+    }
+}
