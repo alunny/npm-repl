8dcbf3c7f3f1b3f293ba70ff748441775a8a204f
alunny <alunny@gmail.com>
2012-11-09 16:24:07 -0800

    basic static page

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(8000);
+var app = require('tako')(),
+    path = require('path')
+
+app.route('/').files(path.join(__dirname, 'static', 'index.html'));
+app.route('*').files(path.join(__dirname, 'static'));
+
+app.httpServer.listen(8000)
 
 console.log('Server running at http://0.0.0.0:8000/');
