8fa1900cd0c6a12e86cbba211927e2621defd5a3
alunny <alunny@gmail.com>
2012-11-10 13:58:52 -0800

    bundle - now installs missing modules

--- bundle.js
+++ bundle.js
@@ -1,17 +1,47 @@
+// bundle a module for the browser, given bundle id
 var fs = require('fs'),
     path = require('path'),
-    browserify = require('browserify');
+    browserify = require('browserify'),
+    npm = require('npm');
 
-module.exports = function bundle(mod) {
-    // need a file to run browserify
-    var entryPath = path.join(__dirname, 'entry.js'),
-        outputPath = path.join(__dirname, 'output.js'),
-        b = browserify();
+module.exports = function bundle(mod, cb) {
+    function bundleOutput() {
+        var entryPath = path.join(__dirname, 'entry.js'),
+            outputPath = path.join(__dirname, mod + '-output.js'),
+            b = browserify();
 
-    fs.writeFileSync(entryPath, 'var ' + mod +
-                                    ' = require("' + mod + '");\n');
+        fs.writeFileSync(entryPath, 'window.' + mod +
+                                        ' = require("' + mod + '");\n');
 
-    b.addEntry(entryPath);
+        b.addEntry(entryPath);
 
-    fs.writeFileSync(outputPath, b.bundle());
+        fs.writeFileSync(outputPath, b.bundle());
+        fs.unlinkSync(entryPath);
+
+        process.nextTick(function () {
+            cb(null, outputPath);
+        });
+    }
+
+    // first off - resolve dependency
+    try {
+        require(mod);
+        process.nextTick(bundleOutput);
+    } catch (e) {
+        console.log(mod + ' not installed')
+
+        npm.load({ loglevel: 'silent' }, function (err) {
+            if (err) {
+                throw err
+            }
+
+            npm.commands.install([mod], function (err, data) {
+                if (err) {
+                    throw err;
+                }
+
+                process.nextTick(bundleOutput);
+            });
+        })
+    }
 }
